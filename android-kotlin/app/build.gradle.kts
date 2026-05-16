c8e2e53866e25a284567347dd88f115da25595f1
nadeemk2050 <nadeemalsaham@gmail.com>
2026-05-16 15:32:56 +0400

    Use runner Android SDK path for CI APK build

--- android-kotlin/app/build.gradle.kts
+++ android-kotlin/app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.assigntask.app"
-    compileSdk = 35
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.assigntask.app"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
     }
